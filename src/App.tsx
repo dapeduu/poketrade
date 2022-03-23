e0f83cc136205cf0e0cd0151e6263b36808d991b
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-03-23 12:08:00 -0300

    ♻️ extract hook to handle pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import { Pagination } from "@mantine/core";
-import { usePagination } from "@mantine/hooks";
-import { useState } from "react";
-import { useQuery } from "react-query";
-import { getPokemons } from "./config/pokenode";
+import { usePokemons } from "./hooks/usePokemons";
 
 function App() {
-  const limit = 6;
-  const [page, setPageState] = useState(1);
-
-  const { data } = useQuery(
-    ["pokemons", page],
-    () => getPokemons(limit, page),
-    { keepPreviousData: true }
-  );
-  const { amountOfPages, pokemons } = data ?? {};
-
-  const { setPage } = usePagination({ total: amountOfPages ?? 0 });
-
-  function changePage(page: number) {
-    setPage(page);
-    setPageState(page);
-  }
+  const { pokemons, amountOfPages, changePage } = usePokemons();
 
   return (
     <div className="App">
---
+++ src/hooks/usePokemons.tsx
@@ -0,0 +1,29 @@
+import { usePagination } from "@mantine/hooks";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { getPokemons } from "../config/pokenode";
+
+export function usePokemons() {
+  const limit = 6;
+  const [page, setPageState] = useState(1);
+
+  const { data } = useQuery(
+    ["pokemons", page],
+    () => getPokemons(limit, page),
+    { keepPreviousData: true }
+  );
+  const { amountOfPages, pokemons } = data ?? {};
+
+  const { setPage } = usePagination({ total: amountOfPages ?? 0 });
+
+  function changePage(page: number) {
+    setPage(page);
+    setPageState(page);
+  }
+
+  return {
+    changePage,
+    pokemons,
+    amountOfPages,
+  };
+}
