57df5f82cfd7bc45dc0aed70770658041c086280
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-03-23 17:19:47 -0300

    ➕ adds pokeball icon svg

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,40 @@
-import { Title, TextInput, Button, Box } from "@mantine/core";
+import { TextInput, Button, Box, Container, AspectRatio } from "@mantine/core";
 import { Clock } from "react-feather";
+// @ts-ignore
+import PokeballIcon from "../assets/pokeball.svg?component";
 
 export function Navbar() {
   return (
     <Box
       sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
+        width: "100%",
         background: "white",
         borderBottom: "1px solid #CED4DA",
       }}
-      py="1rem"
-      px=".5rem"
     >
-      <Title>Poketrade</Title>
-      <TextInput placeholder="Pesquisar Pokémon" />
-      <Button leftIcon={<Clock />} variant="outline">
-        Histórico
-      </Button>
+      <Container
+        size="lg"
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+        py=".5rem"
+        px=".5rem"
+      >
+        <Box
+          sx={{
+            width: 55,
+          }}
+        >
+          <PokeballIcon />
+        </Box>
+
+        <TextInput placeholder="Pesquisar Pokémon" />
+        <Button leftIcon={<Clock />} variant="outline">
+          Histórico
+        </Button>
+      </Container>
     </Box>
   );
 }
